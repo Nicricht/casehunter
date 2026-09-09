80ebf4079b48e631af34994d3988388cc785d2b0
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-09 18:45:53 -0300

    chore: exponer motores de decisión v3

--- casehunter/engines/__init__.py
+++ casehunter/engines/__init__.py
@@ -1,5 +1,15 @@
 """Independent decision engines for Case Hunter."""
 
 from .contact_trust import ContactAssessment, assess_contact
+from .outreach_policy import OutreachDecision, evaluate_first_contact
+from .reply_intelligence import ReplyDecision, analyze_reply, classify_reply
 
-__all__ = ["ContactAssessment", "assess_contact"]
+__all__ = [
+    "ContactAssessment",
+    "OutreachDecision",
+    "ReplyDecision",
+    "assess_contact",
+    "evaluate_first_contact",
+    "analyze_reply",
+    "classify_reply",
+]
