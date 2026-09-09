adabd6d723a87233925d254aa4dcef3ef50ea09d
Nicolás Iván Vega Linero <nic.vegal@duocuc.cl>
2026-09-09 12:28:32 -0300

    feat: migrar script Linux

---
+++ run_linux.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+set -eu
+cd "$(dirname "$0")"
+if [ ! -d .venv ]; then python3 -m venv .venv; fi
+. .venv/bin/activate
+python -m pip install -r requirements.txt
+python -m casehunter verify
+python -m casehunter serve
